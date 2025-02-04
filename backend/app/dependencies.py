cf6a2f9cf8f635c8c7219e73982d14b3cfc0ab3f
Ferrox <nordmanniko@gmail.com>
2025-02-04 13:39:15 +0200

    slowly doing security features

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,8 +1,15 @@
 from typing import Annotated
-
 from sqlmodel import Session, create_engine, SQLModel
 
+import os
+from dotenv import load_dotenv
+
 engine = create_engine("mysql+mysqldb://root:mypass123@localhost/hirvinotes")
+load_dotenv()
+
+key = os.getenv('SECRET_KEY')
+algo = os.getenv('ALGORITHM')
+exp = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -10,3 +17,11 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
+
+def get_env(method):
+    if method == "key":
+        return key
+    elif method == "algo":
+        return algo
+    elif method == "exp":
+        return exp
